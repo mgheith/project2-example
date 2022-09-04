e5b71c2b616ffd07d2dfbfef1d9693583b6188fa
Your Name <you@example.com>
2022-09-04 14:24:05 -0400

    added files for an example baseline implementation

---
+++ lib/game.rb
@@ -0,0 +1,17 @@
+require_relative 'player_creator'
+
+class Game
+
+    def initialize
+        @player_creator = PlayerCreator.new
+    end
+
+    def add_players
+        @players = @player_creator.get_players
+        puts "Welcome #{@players.join(' and ')}"
+    end
+
+    def start
+    end
+
+end
